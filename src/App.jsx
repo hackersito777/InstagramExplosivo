9a69a9412ae222066b428a92f9357a1cc7345a2f
hackersito777 <yefersonjmo777@gmail.com>
2023-12-29 09:55:43 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import SubscribeForm from './components/SubscribeForm';
+import imagenlibro from "./components/imagenlibro.jpg";
+
+const AppContainer = styled.div`
+font-family: 'Roboto', sans-serif;
+text-align: center;
+color: #fff;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+min-height: 100vh;
+`;
+
+const Header = styled.h1`
+color: #4caf50;
+font-size: 10px;
+
+`;
+
+
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Header>
+      
+      
+<h1>
+“EL LIBRO SECRETO DE INSTAGRAM”:
+ESTRATEGIAS PARA LA EXPLOSIÓN EN REDES SOCIALES"
+</h1>
+
+<img src={imagenlibro}></img>
+
+</Header>
+      
+      <SubscribeForm />
+    </AppContainer>
+  );
+};
+
+export default App;
---
+++ src/components/SubscribeForm.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const FormContainer = styled.div`
+  background-color: #121212;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 20px rgba(255, 255, 255, 0.1);
+  text-align: center;
+  max-width: 400px;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #444;
+  border-radius: 4px;
+  background-color: #333;
+  color: #fff;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const SubscribeForm = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubscribe = async () => {
+    try {
+      const response = await fetch('https://formspree.io/f/mdoqooqd', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        console.log('Suscrito con éxito');
+        setEmail('');
+      } else {
+        console.error('Error al suscribirse');
+      }
+    } catch (error) {
+      console.error('Error al enviar la solicitud', error);
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h2 style={{ color: '#4caf50' }}>¿A Que Correo Te Enviamos El Ebook?</h2>
+      <Input
+        type="email"
+        placeholder="Tu correo electrónico"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button onClick={handleSubscribe}>Suscribirse</Button>
+    </FormContainer>
+  );
+};
+
+export default SubscribeForm;
